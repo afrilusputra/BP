cabc015bb7a603a23d2fef2540145b8493fb86a2
unknown <agusaffrilusputra@gmail.com>
2021-10-30 14:47:04 +0700

    test

---
+++ contoh_1.py
@@ -0,0 +1,27 @@
+print("hello world") # ini adalah komentar
+
+'''
+ini komentar 1
+ini komentar 2
+'''
+
+x = "hai"
+y = "Helooooo"
+
+print(x + " " + y)
+
+logika = True
+print(type(logika))
+
+bill = 1.0
+print(type(bill))
+
+x = 1
+y = 2
+z = 3
+
+print ("x + y + z", x + y * z)
+
+x = 2.8
+
+print (int(x))
